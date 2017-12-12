101ca99defb1a2cf673741537ad8bda9dcaffa02
herrBez <mirko.bez@studenti.unipd.it>
2017-12-12 21:39:03 +0100

    Eliminate all files that does not belong to the Ada code base

--- C++/header/utilities.hpp
+++
@@ -1,6 +0,0 @@
-#ifndef __UTILITIES__H__
-#define __UTILITIES__H__
-#include "../header/MOS6502.hpp"
-int parseFriendlyFile(char * filename);
-unsigned char * loadGameFromFriendlyFile(int game_length, char * filename);
-#endif
