5ea9912d78fdc9dc14992dcd214f2c224506ed6b
herrBez <mirko.bez@studenti.unipd.it>
2017-12-23 19:11:15 +0100

    Use Ada.Real_Time package to emulate clock (1MHz) Use package Ada.Real_Time to simulate the clock of MOS6502 (1Mhz - 3MHz), using the delay_until approach

--- Ada/src/main_loop.adb
+++ Ada/src/main_loop.adb
@@ -1,14 +1,18 @@
 package body Main_Loop is
 
 procedure Main_Loop_f (This : in out MOS_T; Instruction : in Fun_Array) is
-
-	begin
+    Next_Release : Ada.Real_Time.Time := 0; 
+    Release_Interval : constant Ada.Real_Time.Time_Span := Ada.Real_Time.Microseconds (1); -- 1 MHz
+    
+    begin
+        Next_Release :=  The_Clock + Release_Interval;
         While_Loop :
             while This.Mem(This.PC) /= 0 loop
                 Print_Status(This);
                 Instruction(This.Mem(This.PC))(This);
-                Print_Status(This);
                 This.PC := This.PC + 2;
+                delay until Next_Release;
+                Next_Release := Next_Release + Release_Interval;
             end loop While_Loop;
     end Main_Loop_f;
 
--- Ada/src/main_loop.ads
+++ Ada/src/main_loop.ads
@@ -1,8 +1,11 @@
 with Ada.Text_IO; use Ada.Text_IO;
 with MOS; use MOS;
 with Instruction; use Instruction;
-
+with Ada.Real_Time; 
 
 package Main_Loop is
+    use type Ada.Real_Time.Time;
+    use type Ada.Real_Time.Time_Span;
+    The_Clock : Ada.Real_Time.Time := Ada.Real_Time.Clock;
     procedure Main_Loop_f (This : in out MOS_T; Instruction : in Fun_Array);
 end Main_Loop;
