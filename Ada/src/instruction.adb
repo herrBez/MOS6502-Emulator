72e26dcbf667e1feecd3e500e3a22b95343db8f1
herrBez <mirko.bez@studenti.unipd.it>
2017-12-23 21:38:27 +0100

    Add stub for ADC and AND functions

--- Ada/src/instruction.adb
+++ Ada/src/instruction.adb
@@ -14,10 +14,87 @@ package body Instruction is
 		New_Line;
     end Not_Yet_Supported_Operation;
     
-    procedure And_Imm(This : in out MOS_T) is
+    -----------------------------------------
+    -- ADC FUNCTIONS
+    -----------------------------------------
+    procedure Adc_Immediate (This : in out MOS_T) is
         C : Byte_T := This.Mem(This.PC+1);
     begin
         This.A := This.A and C;
-	end And_Imm;
+	end Adc_Immediate;
+    
+    procedure Adc_Absolute (This : in out MOS_T) is
+    begin
+        null;
+    end Adc_Absolute;
+    
+    procedure Adc_Zero_Page (This : in out MOS_T) is
+    begin
+        null;
+    end Adc_Zero_Page;
+    
+    procedure Adc_Absolute_X (This : in out MOS_T) is
+    begin
+        null;
+    end Adc_Absolute_X;
+    
+    procedure Adc_Absolute_Y (This : in out MOS_T) is
+    begin
+        null;
+    end Adc_Absolute_Y;
+    
+    procedure Adc_Zero_Page_X_Indexed_Indirect (This : in out MOS_T) is
+    begin
+        null;
+    end Adc_Zero_Page_X_Indexed_Indirect;
+    
+    procedure Adc_Zero_Page_X_Indirect_Indexed  (This : in out MOS_T) is
+    begin
+        null;
+    end Adc_Zero_Page_X_Indirect_Indexed;
+    
+    -----------------------------------------
+    -- AND FUNCTIONS
+    -----------------------------------------
+    procedure And_Immediate (This : in out MOS_T) is
+        C : Byte_T := This.Mem(This.PC+1);
+    begin
+        This.A := This.A and C;
+        
+	end And_Immediate;
+    
+    procedure And_Absolute (This : in out MOS_T) is
+    begin
+        null;
+    end And_Absolute;
+    
+    procedure And_Zero_Page (This : in out MOS_T) is
+    begin
+        null;
+    end And_Zero_Page;
+    
+    procedure And_Absolute_X (This : in out MOS_T) is
+    begin
+        null;
+    end And_Absolute_X;
+    
+    procedure And_Absolute_Y (This : in out MOS_T) is
+    begin
+        null;
+    end And_Absolute_Y;
+    
+    procedure And_Zero_Page_X_Indexed_Indirect (This : in out MOS_T) is
+    begin
+        null;
+    end And_Zero_Page_X_Indexed_Indirect;
+    
+    procedure And_Zero_Page_X_Indirect_Indexed (This : in out MOS_T) is
+    begin
+        null;
+    end And_Zero_Page_X_Indirect_Indexed;
+    
+    
+    
+    
 
 end Instruction;
--- Ada/src/instruction.ads
+++ Ada/src/instruction.ads
@@ -12,5 +12,26 @@ function Initialize_Instruction_Table return Fun_Array;
 
 private 
     procedure Not_Yet_Supported_Operation(This : in out MOS_T);
-    procedure And_Imm(This : in out MOS_T);
+    
+    -----------------------------------------
+    -- ADC FUNCTIONS
+    -----------------------------------------
+    procedure Adc_Immediate (This : in out MOS_T);
+    procedure Adc_Absolute (This : in out MOS_T);
+    procedure Adc_Zero_Page (This : in out MOS_T);
+    procedure Adc_Absolute_X (This : in out MOS_T);
+    procedure Adc_Absolute_Y (This : in out MOS_T);
+    procedure Adc_Zero_Page_X_Indexed_Indirect (This : in out MOS_T);
+    procedure Adc_Zero_Page_X_Indirect_Indexed (This : in out MOS_T);
+    -----------------------------------------
+    -- AND FUNCTIONS
+    -----------------------------------------
+    procedure And_Immediate (This : in out MOS_T);
+    procedure And_Absolute (This : in out MOS_T);
+    procedure And_Zero_Page (This : in out MOS_T);
+    procedure And_Absolute_X (This : in out MOS_T);
+    procedure And_Absolute_Y (This : in out MOS_T);
+    procedure And_Zero_Page_X_Indexed_Indirect (This : in out MOS_T);
+    procedure And_Zero_Page_X_Indirect_Indexed  (This : in out MOS_T);
+    
 end Instruction;
